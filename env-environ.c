add9e1020fcdd2e0c3124299a1aa99ba5e53f43b
xdJidx <84342160+xdJidx@users.noreply.github.com>
2023-04-24 09:54:07 +0200

    Delete env-environ.c

--- env-environ.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-extern char **environ;
-
-int main() {
-    printf("L'adresse de la variable globale 'environ' est : %p\n", (void *)environ);
-    return 0;
-}
