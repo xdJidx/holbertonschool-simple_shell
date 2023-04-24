a7bf885b3d2d6571dfbff23bf8c23a9c6653b137
xdJidx <bernardjerome59@gmail.com>
2023-04-24 01:40:39 -0700

    Update file main.c for Betty

--- hsh_main.c
+++ hsh_main.c
@@ -1,39 +1,51 @@
 #include "hsh_main.h"
 
-int main()
+/**
+ * main - Loops for simple shell
+ * Return: 0 if sucess
+ *
+ */
+
+int main(void)
 {
-     char* command = NULL;
-     _instructions();
-     while (1)
-     {
-         command = _readCommand();
-         if (command == NULL)
-         {
-             printf("\n");
-             exit(EXIT_SUCCESS);
-         }
-         if (strcmp(command, "exit") == 0)
-         {
-             exit(EXIT_SUCCESS);
-         }
-         pid_t pid = fork();
-         if (pid == -1)
-         {
-             perror("Erreur lors de la création d'un nouveau processus");
-         }
-         else if (pid == 0)
-         {
-             /* Code exécuté dans le processus enfant */
-             _executeCommand(command);
-             exit(EXIT_SUCCESS);
-         }
-         else
-         {
-             /* Code exécuté dans le processus parent */
-             int status;
-             waitpid(pid, &status, 0);
-         }
-         free(command);
-     }
-     return (0);
+	char *command = NULL;
+	int status;
+
+	_instructions();
+
+	while (1)
+	{
+		command = _readCommand();
+
+		if (command == NULL)
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+
+		if (strcmp(command, "exit") == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+
+		pid_t pid = fork();
+
+		if (pid == -1)
+		{
+			perror("Erreur lors de la création d'un nouveau processus");
+		}
+		else if (pid == 0)
+		{
+			/* Code executed on child process */
+			_executeCommand(command);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			/* Code executed on parent process */
+			waitpid(pid, &status, 0);
+		}
+		free(command);
+	}
+	return (0);
 }
