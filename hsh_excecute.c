4a98996fad47be1073e23bdcf8b3ed79252879f6
xdJidx <bernardjerome59@gmail.com>
2023-04-24 01:27:28 -0700

    Update file execute for Betty

--- hsh_excecute.c
+++ hsh_excecute.c
@@ -1,32 +1,38 @@
 #include "hsh_main.h"
 
 /**
- * _executeCommand - permet d'excuter la commande
- * @command: la chaine de character
+ * _executeCommand - Execut the command of user
+ * @command: string of user
  *
  */
 
-void _executeCommand(char* command)
+void _executeCommand(char *command)
 {
-     char* args[100];
-     int index = 0;
-     args[index] = strtok(command, " ");
-     while (args[index] != NULL)
-     {
-         index++;
-         args[index] = strtok(NULL, " ");
-     }
-     args[index] = NULL; /* Nécessaire pour execve() */
-     char* path = getenv("PATH");
-     char* path_copy = strdup(path); /* Nécessaire car strtok() modifie la chaîne originale */
-     char* dir = strtok(path_copy, ":");
-     while (dir != NULL)
-     {
-         char command_path[MAX_COMMAND_SIZE];
-         snprintf(command_path, MAX_COMMAND_SIZE, "%s/%s", dir, args[0]);
-         execve(command_path, args, NULL);
-         dir = strtok(NULL, ":");
-     }
-     printf("Commande inconnue.\n");
-     free(path_copy);
+	char *args[100];
+	int index = 0;
+	char *path, path_copy, dir;
+
+	args[index] = strtok(command, " ");
+	while (args[index] != NULL)
+	{
+		index++;
+		args[index] = strtok(NULL, " ");
+	}
+	args[index] = NULL; /* Use to execve() */
+
+	path = getenv("PATH");
+	path_copy = strdup(path); /* Use for strtok() */
+	dir = strtok(path_copy, ":");
+
+	while (dir != NULL)
+	{
+		char command_path[MAX_COMMAND_SIZE];
+
+		snprintf(command_path, MAX_COMMAND_SIZE, "%s/%s", dir, args[0]);
+		execve(command_path, args, NULL);
+		dir = strtok(NULL, ":");
+	}
+
+	printf("Commande inconnue.\n");
+	free(path_copy);
 }
