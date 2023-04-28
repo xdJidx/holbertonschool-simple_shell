83f9c7ecda2e3615377aec10d8c437fb860dd693
xdJidx <bernardjerome59@gmail.com>
2023-04-28 01:57:58 -0700

    Add new functions and corrected

--- hsh_main.h
+++ hsh_main.h
@@ -8,19 +8,22 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
 
 #define MAX_COMMAND_SIZE 100
 
+extern char **environ;
 void _instructions(void);
 char *_readCommand(void);
-void _executeCommand(char *command);
+void _executeCommand(char **argv);
 char *_strdup(char *str);
 int _strlen(char *str);
 int _strcmp(char *s1, char *s2);
 char *_getenv(char *name);
 char *_strchr(const char *str, int c);
 char **tokenEnv(char *path);
-char tokenCommand(char *command);
+char **tokenCommand(char *command);
 char *findPath(char *command);
 char *_strcat(char *dest, char *src);
 
