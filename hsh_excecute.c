45fbb2d4c3d404d96addc913f15b54be6847fea0
ananas122 <melle.noraagmail.com>
2023-04-28 05:54:40 -0700

    Add acces condition for builtin

--- hsh_excecute.c
+++ hsh_excecute.c
@@ -8,23 +8,40 @@
 void _executeCommand(char **argv)
 {
 	char *command = argv[0];
-	/* If the command is not in the path, look for it */
-	if (command[0] == '/' || command[0] == '.')
-	{
-		command = argv[0];
-	}
-	else
-		command = findPath(argv[0]);
+	int i = 0;
+	int isBuiltIn = 0;
 
-	/* If the command is still not found, print an error message */
-	if (command == NULL)
-	{
-		free(command);
-		perror("Error: no command found");
+	while (builtins[i].name != NULL)
+    	{
+		if (strcmp(command, builtins[i].name) == 0)
+		{
+			builtins[i].func(argv);
+			isBuiltIn = 1;
+			break;
+		}
+		i++;
 	}
-	/* Execute the command */
-	if (execve(command, argv, environ) == -1)
-	{
-		perror("Error: execve");
+	/*If the command is not built-in, look for it in the path and execute it*/
+	if (!isBuiltIn)
+    	{
+		/* If the command is not in the path, look for it */
+		if (command[0] == '/' || command[0] == '.')
+		{
+			command = argv[0];
+		}
+		else
+			command = findPath(argv[0]);
+
+		/* If the command is still not found, print an error message */
+		if (command == NULL)
+		{
+			free(command);
+			perror("Error: no command found");
+		}
+		/* Execute the command */
+		if (execve(command, argv, environ) == -1)
+		{
+			perror("Error: execve");
+		}
 	}
 }
