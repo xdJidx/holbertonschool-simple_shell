c0e9bd61f5cf4675edc2bdead2947fdbd2b76e0f
ananas122 <melle.noraagmail.com>
2023-04-28 01:32:45 -0700

    Create file to tokenize command

---
+++ token_command.c
@@ -0,0 +1,39 @@
+#include "hsh_main.h"
+
+/**
+ * tokenize - tokenize the line
+ * @line: line to be tokenized
+ *
+ * Return: argv pointer
+*/
+
+char **tokenCommand(char *command)
+{
+        int token_size = 64;/*taille initiale allouée pour argv*/
+        char *tokenCommand = NULL;
+        char **argv = NULL;
+        int index = 0;
+
+        argv = malloc(token_size * sizeof(char *));/* allocation de la mémoire pour argv */
+
+        if (argv == NULL)/*Vérifie si l'allocation de mémoire a réussi*/
+        {
+                perror("Error : allocation memory");/*Affiche un mess d'erreur et quitte le programmm
+e*/
+                free(argv);
+                free(command);
+                exit(1);
+        }
+        token = strtok(command, " ");
+
+        while (tokenCommand != NULL)
+        {
+                argv[index] = token;/* stocke le token dans le tableau argv */
+                index++;
+                tokenCommand = strtok(NULL, " ");/* passe au token suivant */
+        }
+        argv[index] = NULL;/* ajoute un élément NULL pour marquer la fin du tableau */
+
+        return (argv);/* retourne un pointeur vers le tableau de tokens */
+}
+
