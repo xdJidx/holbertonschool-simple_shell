1d4ce0419ad50e95ec8f6acbe2abe44815887a12
ananas122 <melle.noraagmail.com>
2023-04-28 03:24:40 -0700

    Correction Betty and add comments for to describe the steps

--- hsh_excecute.c
+++ hsh_excecute.c
@@ -2,29 +2,29 @@
 
 /**
  * _executeCommand - Execut the command of user
- * @command: string of user use
- *
+ * @argv: array of strings containing the user's command and arguments
  */
 
 void _executeCommand(char **argv)
 {
 	char *command = argv[0];
-
+i	/* If the command is not in the path, look for it */
 	if (command[0] == '/' || command[0] == '.')
 	{
-		command = argv[0];	
+		command = argv[0];
 	}
 	else
 		command = findPath(argv[0]);
 
+	/* If the command is still not found, print an error message */
 	if (command == NULL)
-		{
-			free(command);
-			perror("Error: no command found");
-		}
-
+	{
+		free(command);
+		perror("Error: no command found");
+	}
+	/* Execute the command */
 	if (execve(command, argv, environ) == -1)
-		{
-			perror("Error: execve");
-		}
+	{
+		perror("Error: execve");
+	}
 }
