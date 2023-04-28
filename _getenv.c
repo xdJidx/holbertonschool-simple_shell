c629af626d569d85a44470fd30354b6ad864013b
xdJidx <bernardjerome59@gmail.com>
2023-04-28 02:02:29 -0700

    Correction of variable name

--- _getenv.c
+++ _getenv.c
@@ -7,18 +7,20 @@
  * Return: NULL.
  */
 
-char _getenv(char *command)
+char *_getenv(char *command)
 {
         int index, size;
+	char **env = environ;
+
 
         size = strlen(command);/* calculer la taille de la commande */
         /* parcourir la liste d'environnement jusqu'à sa fin */
-        for (index = 0; environ[index]; index++)
+        for (index = 0; env[index]; index++)
         {
                 /* comparer les premiers caractères de la commande avec chaque var d'env */
-                if (strncmp(environ[index], command, size) == 0)
+                if (strncmp(env[index], command, size) == 0)
                 /*Retourne la val de la var d'env à partir de la position après le nom de la var */
-                return (environ[index] + size + 1);
+                return (env[index] + size + 1);
         }
         /* Retourne NULL si la variable d'environnement n'a pas été trouvée */
         return (NULL);
