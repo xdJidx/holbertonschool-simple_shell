b7131af62a05e7dbb1152541c026675e3f0ec10a
ananas122 <melle.noraagmail.com>
2023-04-28 02:21:23 -0700

    Correction Betty espaces et tabulation

--- _getenv.c
+++ _getenv.c
@@ -9,20 +9,20 @@
 
 char *_getenv(char *command)
 {
-        int index, size;
+	int index, size;
 	char **env = environ;
 
-
-        size = strlen(command);/* calculer la taille de la commande */
-        /* parcourir la liste d'environnement jusqu'à sa fin */
-        for (index = 0; env[index]; index++)
-        {
-                /* comparer les premiers caractères de la commande avec chaque var d'env */
-                if (strncmp(env[index], command, size) == 0)
-                /*Retourne la val de la var d'env à partir de la position après le nom de la var */
-                return (env[index] + size + 1);
-        }
-        /* Retourne NULL si la variable d'environnement n'a pas été trouvée */
-        return (NULL);
+	size = strlen(command);/* calculer la taille de la commande */
+	/* parcourir la liste d'environnement jusqu'à sa fin */
+	for (index = 0; env[index]; index++)
+	{
+	/* comparer les premiers caractères de la commande avec chaque var d'env */
+		if (strncmp(env[index], command, size) == 0)
+			/* si la commande correspond à une var d'env,*/
+			/* return la valeur de la var d'env sans le nom de la commande */
+			return (env[index] + size + 1);
+	}
+	/* Retourne NULL si la variable d'environnement n'a pas été trouvée */
+	return (NULL);
 }
 
