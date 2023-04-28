d76632e563ce57ee1be1acd4c017c391e1e8d79f
ananas122 <melle.noraagmail.com>
2023-04-28 02:50:09 -0700

    Correction Betty deletes spaces and tab and add comments

--- token_command.c
+++ token_command.c
@@ -1,39 +1,40 @@
 #include "hsh_main.h"
 
 /**
- * tokenize - tokenize the line
- * @line: line to be tokenized
+ * tokenCommand - tokenize the line
+ * @command: command to be tokenized
  *
  * Return: argv pointer
 */
 
 char **tokenCommand(char *command)
 {
-        int token_size = 64;/*taille initiale allouée pour argv*/
-        char *tokenCommand = NULL;
-        char **argv = NULL;
-        int index = 0;
+	int token_size = 64;/*taille initiale allouée pour argv*/
+	char *tokenCommand = NULL;
+	char **argv = NULL;
+	int index = 0;
 
-        argv = malloc(token_size * sizeof(char *));/* allocation de la mémoire pour argv */
+	argv = malloc(token_size * sizeof(char *));
+	/* allocation de la mémoire pour argv */
 
-        if (argv == NULL)/*Vérifie si l'allocation de mémoire a réussi*/
-        {
-                perror("Error : allocation memory");/*Affiche un mess d'erreur et quitte le programmm
-e*/
-                free(argv);
-                free(command);
-                exit(1);
-        }
-        tokenCommand = strtok(command, " ");
+	if (argv == NULL)/*Vérifie si l'allocation de mémoire a réussi*/
+	{
+		/*Affiche un mess d'erreur et quitte le prog*/
+		perror("Error : allocation memory");
+		free(argv);
+		free(command);
+		exit(1);
+	}
+	tokenCommand = strtok(command, " ");
 
-        while (tokenCommand != NULL)
-        {
-                argv[index] = tokenCommand;/* stocke le token dans le tableau argv */
-                index++;
-                tokenCommand = strtok(NULL, " ");/* passe au token suivant */
-        }
-        argv[index] = NULL;/* ajoute un élément NULL pour marquer la fin du tableau */
+	while (tokenCommand != NULL)
+	{
+		argv[index] = tokenCommand;/* stocke le token dans le tableau argv */
+		index++;
+		tokenCommand = strtok(NULL, " ");
+		/* passe au token suivant */
+	}
+	argv[index] = NULL;/* ajoute un élément NULL pr marquer la fin du tabl */
 
-        return (argv);/* retourne un pointeur vers le tableau de tokens */
+	return (argv);/* retourne un pointeur vers le tableau de tokens */
 }
-
